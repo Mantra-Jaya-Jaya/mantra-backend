b4b561b4e9ad3c21168ffa458e898d2258ef4963
Terra Surya Negari <suryaterra88@gmail.com>
2026-05-10 22:22:46 +0700

    feat: add atlas schema configuration and document entity relationships

--- models/kasir.go
+++ models/kasir.go
@@ -8,14 +8,17 @@ func (Kasir) TableName() string {
 }
 
 type Kasir struct {
-	IdKasir      uint      `gorm:"primaryKey;column:id_kasir"`
-	NoTelp       string    `gorm:"column:no_telp"`
-	Alamat       string    `gorm:"type:text;column:alamat"`
-	TanggalLahir time.Time `gorm:"type:date;column:tanggal_lahir"`
-	Foto         string    `gorm:"column:foto"`
-	Shift        string    `gorm:"column:shift"` // Pagi | Siang | Malam
-	CreatedAt    time.Time `gorm:"column:created_at"`
-	UpdatedAt    time.Time `gorm:"column:updated_at"`
+	IdKasir            uint      `gorm:"primaryKey;column:id_kasir"`
+	NoTelp             string    `gorm:"column:no_telp"`
+	Alamat             string    `gorm:"type:text;column:alamat"`
+	TanggalLahir       time.Time `gorm:"type:date;column:tanggal_lahir"`
+	Foto               string    `gorm:"column:foto"`
+	JenisKelamin       string    `gorm:"column:jenis_kelamin"`
+	PendidikanTerakhir string    `gorm:"column:pendidikan_terakhir"`
+	Nik                string    `gorm:"column:nik"`
+	Shift              string    `gorm:"column:shift"` // Pagi | Siang | Malam
+	CreatedAt          time.Time `gorm:"column:created_at"`
+	UpdatedAt          time.Time `gorm:"column:updated_at"`
 
 	// Relasi ke User
 	UserId uint `gorm:"column:id_user;unique"`
--- models/kurir.go
+++ models/kurir.go
@@ -8,13 +8,17 @@ func (Kurir) TableName() string {
 }
 
 type Kurir struct {
-	IdKurir      uint      `gorm:"primaryKey;column:id_kurir"`
-	NoTelp       string    `gorm:"column:no_telp"`
-	Alamat       string    `gorm:"type:text;column:alamat"`
-	TanggalLahir time.Time `gorm:"type:date;column:tanggal_lahir"`
-	Foto         string    `gorm:"column:foto"`
-	CreatedAt    time.Time `gorm:"column:created_at"`
-	UpdatedAt    time.Time `gorm:"column:updated_at"`
+	IdKurir            uint      `gorm:"primaryKey;column:id_kurir"`
+	NoTelp             string    `gorm:"column:no_telp"`
+	Alamat             string    `gorm:"type:text;column:alamat"`
+	TanggalLahir       time.Time `gorm:"type:date;column:tanggal_lahir"`
+	JenisKelamin       string    `gorm:"column:jenis_kelamin"`
+	PendidikanTerakhir string    `gorm:"column:pendidikan_terakhir"`
+	TempatLahir        string    `gorm:"column:tempat_lahir"`
+	Nik                string    `gorm:"type:varchar(16);column:nik;unique;not null"`
+	Foto               string    `gorm:"column:foto"`
+	CreatedAt          time.Time `gorm:"column:created_at"`
+	UpdatedAt          time.Time `gorm:"column:updated_at"`
 
 	// Relasi ke User
 	UserId uint `gorm:"column:id_user;unique"`
